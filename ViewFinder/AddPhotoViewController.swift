e48ca0576668a0c4624f8024c3321978dae73220
Apple <apple@Apples-MacBook-Pro-106.local>
2019-06-12 09:27:53 -0500

    Commit Iteration 1

--- ViewFinder/AddPhotoViewController.swift
+++ ViewFinder/AddPhotoViewController.swift
@@ -21,15 +21,37 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
         // Do any additional setup after loading the view.
     }
     
-    internal func imagePickerController(_ _picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            newImage.image = selectedImage
-        }
-        imagePicker.dismiss(animated: true, completion: nil)
+    @IBAction func cameraButton(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
         
+        present(imagePicker, animated: true, completion: nil)
     }
     
-
+    
+    @IBAction func photoLibraryButton(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+        imagePicker.sourceType = .savedPhotosAlbum
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func albumButton(_ sender: UIButton) {
+//        imagePicker.sourceType = .
+//
+//        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
+    
+    @IBOutlet weak var imageOutlet: UIImageView!
     /*
     // MARK: - Navigation
 
@@ -39,5 +61,12 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
         // Pass the selected object to the new view controller.
     }
     */
+    internal func imagePickerController(_ _picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageOutlet.image = selectedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
 
 }
